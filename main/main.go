701d8b6cf62c5ca607ed8bc4a1aded439f293f50
Shao Jie <zhang1591313226@163.com>
2020-10-29 00:04:33 +0800

    学习渲染数据

--- main/main.go
+++ main/main.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"net/http"
+	"log"
+	"time"
 )
 
 //func main() {
@@ -88,34 +89,103 @@ import (
 //	}
 //}
 
-// 定义接收数据的结构体
-type Login struct {
-	// binding:"required"修饰的字段，若接收为空值，则报错，是必须字段
-	User    string `form:"username" json:"user" uri:"user" xml:"user" binding:"required"`
-	Pssword string `form:"password" json:"password" uri:"password" xml:"password" binding:"required"`
-}
+//// 定义接收数据的结构体
+//type Login struct {
+//	// binding:"required"修饰的字段，若接收为空值，则报错，是必须字段
+//	User    string `form:"username" json:"user" uri:"user" xml:"user" binding:"required"`
+//	Pssword string `form:"password" json:"password" uri:"password" xml:"password" binding:"required"`
+//}
+//
+//func main() {
+//	// 1.创建路由
+//	// 默认使用了2个中间件Logger(), Recovery()
+//	r := gin.Default()
+//	// JSON绑定
+//	r.POST("loginJSON", func(c *gin.Context) {
+//		// 声明接收的变量
+//		var json Login
+//		// 将request的body中的数据，自动按照json格式解析到结构体
+//		if err := c.ShouldBindJSON(&json); err != nil {
+//			// 返回错误信息
+//			// gin.H封装了生成json数据的工具
+//			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+//			return
+//		}
+//		// 判断用户名密码是否正确
+//		if json.User != "root" || json.Pssword != "admin" {
+//			c.JSON(http.StatusBadRequest, gin.H{"status": "304"})
+//			return
+//		}
+//		c.JSON(http.StatusOK, gin.H{"status": "200"})
+//	})
+//	r.Run(":8000")
+//}
+
+//// 多种响应方式
+//func main() {
+//	// 1.创建路由
+//	// 默认使用了2个中间件Logger(), Recovery()
+//	r := gin.Default()
+//	// 1.json
+//	r.GET("/someJSON", func(c *gin.Context) {
+//		c.JSON(200, gin.H{"message": "someJSON", "status": 200})
+//	})
+//	// 2. 结构体响应
+//	r.GET("/someStruct", func(c *gin.Context) {
+//		var msg struct {
+//			Name    string
+//			Message string
+//			Number  int
+//		}
+//		msg.Name = "root"
+//		msg.Message = "message"
+//		msg.Number = 123
+//		c.JSON(200, msg)
+//	})
+//	// 3.XML
+//	r.GET("/someXML", func(c *gin.Context) {
+//		c.XML(200, gin.H{"message": "abc"})
+//	})
+//	// 4.YAML响应
+//	r.GET("/someYAML", func(c *gin.Context) {
+//		c.YAML(200, gin.H{"name": "zhangsan"})
+//	})
+//	// 5.protobuf格式,谷歌开发的高效存储读取的工具
+//	// 数组？切片？如果自己构建一个传输格式，应该是什么格式？
+//	r.GET("/someProtoBuf", func(c *gin.Context) {
+//		reps := []int64{int64(1), int64(2)}
+//		// 定义数据
+//		label := "label"
+//		// 传protobuf格式数据
+//		data := &protoexample.Test{
+//			Label: &label,
+//			Reps:  reps,
+//		}
+//		c.ProtoBuf(200, data)
+//	})
+//
+//	r.Run(":8000")
+//}
 
 func main() {
 	// 1.创建路由
 	// 默认使用了2个中间件Logger(), Recovery()
 	r := gin.Default()
-	// JSON绑定
-	r.POST("loginJSON", func(c *gin.Context) {
-		// 声明接收的变量
-		var json Login
-		// 将request的body中的数据，自动按照json格式解析到结构体
-		if err := c.ShouldBindJSON(&json); err != nil {
-			// 返回错误信息
-			// gin.H封装了生成json数据的工具
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-		// 判断用户名密码是否正确
-		if json.User != "root" || json.Pssword != "admin" {
-			c.JSON(http.StatusBadRequest, gin.H{"status": "304"})
-			return
-		}
-		c.JSON(http.StatusOK, gin.H{"status": "200"})
+	// 1.异步
+	r.GET("/long_async", func(c *gin.Context) {
+		// 需要搞一个副本
+		copyContext := c.Copy()
+		// 异步处理
+		go func() {
+			time.Sleep(3 * time.Second)
+			log.Println("异步执行：" + copyContext.Request.URL.Path)
+		}()
 	})
+	// 2.同步
+	r.GET("/long_sync", func(c *gin.Context) {
+		time.Sleep(3 * time.Second)
+		log.Println("同步执行：" + c.Request.URL.Path)
+	})
+
 	r.Run(":8000")
 }
