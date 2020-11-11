fcdbfd83dda76310dcbb78ae216bdd3f867aeaef
Shao Jie <zhang1591313226@163.com>
2020-11-12 00:03:45 +0800

    学习cookie

--- main.go
+++ main.go
@@ -544,3 +544,59 @@ http://127.0.0.1:8080/intermediate
 //func shopHomeHandler(c *gin.Context) {
 //	time.Sleep(3 * time.Second)
 //}
+//func main() {
+//	// 1.创建路由
+//	// 默认使用了2个中间件Logger(), Recovery()
+//	r := gin.Default()
+//	// 服务端要给客户端cookie
+//	r.GET("cookie", func(c *gin.Context) {
+//		// 获取客户端是否携带cookie
+//		cookie, err := c.Cookie("key_cookie")
+//		if err != nil {
+//			cookie = "NotSet"
+//			// 给客户端设置cookie
+//			//  maxAge int, 单位为秒
+//			// path,cookie所在目录
+//			// domain string,域名
+//			//   secure 是否智能通过https访问
+//			// httpOnly bool  是否允许别人通过js获取自己的cookie
+//			c.SetCookie("key_cookie", "value_cookie", 60, "/",
+//				"localhost", false, true)
+//		}
+//		fmt.Printf("cookie的值是： %s\n", cookie)
+//	})
+//	r.Run()
+//}
+
+func AuthMiddleWare() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// 获取客户端cookie并校验
+		if cookie, err := c.Cookie("abc"); err == nil {
+			if cookie == "123" {
+				c.Next()
+				return
+			}
+		}
+		// 返回错误
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "err"})
+		// 若验证不通过，不再调用后续的函数处理
+		c.Abort()
+		return
+	}
+}
+
+func main() {
+	// 1.创建路由
+	r := gin.Default()
+	r.GET("/login", func(c *gin.Context) {
+		// 设置cookie
+		c.SetCookie("abc", "123", 60, "/",
+			"localhost", false, true)
+		// 返回信息
+		c.String(200, "Login success!")
+	})
+	r.GET("/home", AuthMiddleWare(), func(c *gin.Context) {
+		c.JSON(200, gin.H{"data": "home"})
+	})
+	r.Run()
+}
